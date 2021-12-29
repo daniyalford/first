84a9f2cd513e45a19dfc0cf5d574b688692f448a
daniyalford <dfrd19091996@gmail.com>
2021-12-29 15:49:47 +0330

    pfpf

--- application/views/profile.php
+++ application/views/profile.php
@@ -11,62 +11,22 @@
 						<h1><small class="pull-right">45 comments</small> Comments </h1>
 					</div>
 					<div class="comments-list">
-						<div class="media">
-							<p class="pull-right"><small>5 days ago</small></p>
-							<a class="media-left" href="#">
-								<img src="http://lorempixel.com/40/40/people/1/">
-							</a>
-							<div class="media-body">
+						<?php foreach ($data as $tb) { ?>
+							<div class="media">
+								<p class="pull-right"><small><?= //$tb['time'] ?></small></p>
+								<a class="media-left" href="#">
+									<img src="<?= //$tb['pic'] ?>">
+								</a>
+								<div class="media-body">
 
-								<h4 class="media-heading user_name">Baltej Singh</h4>
-								Wow! this is really great.
+									<h4 class="media-heading user_name"><?= $tb['student_name'] ?></h4>
+									<?= $tb['student_description'] ?>
 
-								<p><small><a href="">Like</a> - <a href="">Share</a></small></p>
+									<p><small><a href="">Like</a> - <a href="">Share</a></small></p>
+								</div>
 							</div>
-						</div>
-						<div class="media">
-							<p class="pull-right"><small>5 days ago</small></p>
-							<a class="media-left" href="#">
-								<img src="http://lorempixel.com/40/40/people/2/">
-							</a>
-							<div class="media-body">
-
-								<h4 class="media-heading user_name">Baltej Singh</h4>
-								Wow! this is really great.
-
-								<p><small><a href="">Like</a> - <a href="">Share</a></small></p>
-							</div>
-						</div>
-						<div class="media">
-							<p class="pull-right"><small>5 days ago</small></p>
-							<a class="media-left" href="#">
-								<img src="http://lorempixel.com/40/40/people/3/">
-							</a>
-							<div class="media-body">
-
-								<h4 class="media-heading user_name">Baltej Singh</h4>
-								Wow! this is really great.
-
-								<p><small><a href="">Like</a> - <a href="">Share</a></small></p>
-							</div>
-						</div>
-						<div class="media">
-							<p class="pull-right"><small>5 days ago</small></p>
-							<a class="media-left" href="#">
-								<img src="http://lorempixel.com/40/40/people/4/">
-							</a>
-							<div class="media-body">
-
-								<h4 class="media-heading user_name">Baltej Singh</h4>
-								Wow! this is really great.
-
-								<p><small><a href="">Like</a> - <a href="">Share</a></small></p>
-							</div>
-						</div>
+						<?php } ?>
 					</div>
-
-
-
 				</div>
 			</div>
 		<?php } else { ?>
